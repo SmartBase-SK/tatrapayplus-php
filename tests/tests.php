1eb93c45362a1c853c6a2885c1979093d5c2eaa7
Ľubomír Samotný <lubomir.samotny@smartbase.sk>
2025-04-30 13:06:19 +0200

    triv: use credentials from github secrets

--- tests/tests.php
+++ tests/tests.php
@@ -7,13 +7,23 @@ use Tatrapayplus\TatrapayplusApiClient\TatraPayPlusService;
 
 final class Tests extends TestCase
 {
+    private function getApiCredentials()
+    {
+        $client_id = getenv('TATRAPAY_CLIENT_ID');
+        $client_secret = getenv('TATRAPAY_CLIENT_SECRET');
+
+        return [$client_id, $client_secret];
+    }
+
     public function testRetrieveAccessToken(): void
     {
+        [$client_id, $client_secret] = $this->getApiCredentials();
+
         $client = new MagentoCurlClient(null, null);
         $access_token = TatraPayPlusService::retrieve_access_token_with_credentials(
             $client,
-            'client_id',
-            'client_secret',
+            $client_id,
+            $client_secret,
         );
 
         $this->assertTrue(is_string($access_token));
@@ -21,7 +31,7 @@ final class Tests extends TestCase
 
     public function testLimitLength(): void
     {
-        $result = TatraPayPlusService::limit_length('asdqwe', 3);
-        $this->assertSame($result, 'asd');
+        $result = TatraPayPlusService::limit_length('test123', 4);
+        $this->assertSame($result, 'test');
     }
 }
