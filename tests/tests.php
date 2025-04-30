52fef379f968ee1891c9efcf3ea182a54eb7770b
Ľubomír Samotný <lubomir.samotny@smartbase.sk>
2025-04-30 11:58:44 +0200

    triv: add basic api test

--- tests/tests.php
+++ tests/tests.php
@@ -1,10 +1,27 @@
 <?php declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 
+use Tatrapayplus\TatrapayplusApiClient\MagentoCurlClient;
+use Tatrapayplus\TatrapayplusApiClient\TatraPayPlusService;
+
+
 final class Tests extends TestCase
 {
-    public function testPrint(): void
+    public function testRetrieveAccessToken(): void
     {
-        print('testing my custom test');
+        $client = new MagentoCurlClient(null, null);
+        $access_token = TatraPayPlusService::retrieve_access_token_with_credentials(
+            $client,
+            'l7ba7ffa0bf66b49b88d17dfe144955f54',
+            'b88957109f344ab685a275a5fc9ab39e',
+        );
+
+        $this->assertTrue(is_string($access_token));
+    }
+
+    public function testLimitLength(): void
+    {
+        $result = TatraPayPlusService::limit_length('asdqwe', 3);
+        $this->assertSame($result, 'asd');
     }
 }
